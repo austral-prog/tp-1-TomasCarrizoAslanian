3f9e8c70d4a5f9644de9b1495004dfcfd09709fb
TomasCarrizoAslanian <tcarrizoaslanian@mail.austral.edu.ar>
2025-03-21 11:51:45 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,23 @@
 def math():
     a = 57
     b = 7
+    add = a + b
+    print(add)
+
+    subtract = a - b
+    print(subtract)
+
+    multiply = a * b
+    print(multiply)
+
+    average = (a + b) / 2
+    print(average)
+
+    integer_division = a // b
+    print(integer_division)
+
+    resto = a % b
+    print(resto)
+
+    division = a / b
+    print(division)
